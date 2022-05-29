74d16502971efd6954233a7cc3fa7ee51006d8a8
emiliosp <emilio.spatola@gmail.com>
2022-05-29 17:34:48 +0200

    remove useless feature

---
+++ index.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    jest: true,
+    node: true
+  },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module'
+  },
+  plugins: ['prettier', 'import', 'jest'],
+  extends: ['eslint:recommended', 'prettier', 'plugin:jest/recommended'],
+  rules: {
+    'no-console': 'off',
+    'max-lines-per-function': ['error', 200],
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        printWidth: 160,
+      },
+    ],
+    'import/order': 'error',
+    'import/extensions': ['error', 'ignorePackages']
+  },
+  settings: {
+    jest: {
+      version: 'detect',
+    },
+  },
+};
